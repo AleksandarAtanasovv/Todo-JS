3383925aa9fa2cba940e6afb46e2294a8d3b816d
Sakinaa782 <sashotrupa@hotmail.com>
2022-07-01 16:30:20 +0300

    Final Commit

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const btn = document.getElementById('btn');
-const todo = document.getElementById('inputField');
-const todos = document.getElementById('todos');
+const btn = document.getElementById("btn");
+const todo = document.getElementById("inputField");
+const todos = document.getElementById("todos");
+const todosAmount = document.getElementById("amount");
 
-btn.addEventListener('click', () => {
-    createToDo();
-})
+btn.addEventListener("click", () => {
+  createAndRemoveToDo();
+});
 
-function createAndRemoveToDo(){
-    //Todo Create
-    const newP = document.createElement('p')
-    newP.classList.add('todoParagraph')
-    newP.innerText = todo.value;
-    todos.appendChild(newP)
-    todo.value = "";
-    //Todo Remove
-
-    newP.addEventListener('click', () =>{
-        
-    })
+function createAndRemoveToDo() {
+  //Todo Create
+  const newP = document.createElement("p");
+  newP.classList.add("todoParagraph");
+  newP.innerText = todo.value;
+  todos.appendChild(newP);
+  todo.value = "";
+  //Todo Remove
+  newP.addEventListener("click", () => {
+    newP.remove();
+  });
 }
